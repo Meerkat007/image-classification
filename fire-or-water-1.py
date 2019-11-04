34cc3e767bc812bf67a397deb7ca24f4e36ab432
Yuan <yuanji.yj@gmail.com>
2019-11-03 20:22:55 -0500

    first model

---
+++ fire-or-water-1.py
@@ -0,0 +1,165 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+transform = transforms.Compose(
+    [
+        transforms.Resize(120),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ]
+)
+
+########### Train set ###########
+# Set the directory for the data
+trainset_data_path = './training_set_water_and_fire/train/'
+
+# Use the image folder function to create datasets
+trainset = torchvision.datasets.ImageFolder(
+    root=trainset_data_path,
+    transform=transform
+)
+trainloader = torch.utils.data.DataLoader(
+    trainset,
+    batch_size=4,
+    num_workers=2,
+    shuffle=True
+)
+
+########### Validation set ###########
+validationset_data_path = './training_set_water_and_fire/validation/'
+
+# Use the image folder function to create datasets
+validationset = torchvision.datasets.ImageFolder(
+    root=validationset_data_path,
+    transform=transform
+)
+validationloader = torch.utils.data.DataLoader(
+    trainset,
+    batch_size=4,
+    num_workers=2,
+    shuffle=True
+)
+
+classes = ('Fire', 'Water')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+# dataiter = iter(trainloader)
+# images, labels = dataiter.next()
+
+# # show images
+# imshow(torchvision.utils.make_grid(images))
+# # print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 27 * 27, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 27 * 27)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+########################################################################
+# 3. Define a Loss function and optimizer
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Let's use a Classification Cross-Entropy loss and SGD with momentum.
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+########################################################################
+# 4. Train the network
+# ^^^^^^^^^^^^^^^^^^^^
+#
+# This is when things start to get interesting.
+# We simply have to loop over our data iterator, and feed the inputs to the
+# network and optimize.
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+########################################################################
+# Let's quickly save our trained model:
+
+PATH = './fire_or_water_net.pth'
+torch.save(net.state_dict(), PATH)
+
+# 5. Validate the network on the validation data
+net = Net()
+net.load_state_dict(torch.load(PATH))
+
+dataiter = iter(validationloader)
+images, labels = dataiter.next()
+
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in validationloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the test images: %d %%' % (
+    100 * correct / total))
